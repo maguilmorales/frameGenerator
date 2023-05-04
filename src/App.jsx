91392b82d8caf837231090a7f1f5624ed19224fa
Magali <magali.morales@dialpad.com>
2023-05-04 15:34:49 -0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,64 @@
 import React, { useRef, useEffect } from "react";
 import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
-import { Plane, OrbitControls, Instance, Instances } from "@react-three/drei";
+import { Plane, OrbitControls, Instance, Instances, Line } from "@react-three/drei";
 import * as THREE from "three";
 import { useControls } from "leva";
 import { TextureLoader } from "three/src/loaders/TextureLoader";
 import fragmentShader from "./shaders/fragment.glsl";
 import vertexShader from "./shaders/vertex.glsl";
 import { DoubleSide } from "three";
+import { LineGeometry } from "three-stdlib";
 
 function App() {
+
+  // function RoundedLine(){
+  //   const { width, height, radius } = useControls({
+  //     width: { value: 50, min: 41, max: 100, step: 1 },
+  //     height: { value: 50, min: 41, max: 100, step: 1 },
+  //     radius: { value: 20, min: 1, max: 30, step: 1 },
+  //   });
+    
+  //   const shapeR = new THREE.Shape();
+
+  //   const x = 0;
+  //   const y = 0;
+
+  //   shapeR.moveTo(x, y + radius);
+  //   shapeR.lineTo(x, y + height - radius);
+  //   shapeR.quadraticCurveTo(x, y + height, x + radius, y + height);
+  //   shapeR.lineTo(x + width - radius, y + height);
+  //   shapeR.quadraticCurveTo(
+  //     x + width,
+  //     y + height,
+  //     x + width,
+  //     y + height - radius
+  //   );
+  //   shapeR.lineTo(x + width, y + radius);
+  //   shapeR.quadraticCurveTo(x + width, y, x + width - radius, y);
+  //   shapeR.lineTo(x + radius, y);
+  //   shapeR.quadraticCurveTo(x, y, x, y + radius);
+
+  //   const points = shapeR.getPoints();
+	// 	const geometryPoints = new THREE.BufferGeometry().setFromPoints( points );
+  //   console.log(geometryPoints)
+  //   shapeR.autoClose = true;
+    
+  
+  //   return geometryPoints;
+
+  // }
+
   function RoundedRect() {
+
     const { width, height, radius } = useControls({
       width: { value: 50, min: 41, max: 100, step: 1 },
       height: { value: 50, min: 41, max: 100, step: 1 },
-      radius: { value: 20, min: 1, max: 30, step: 1 },
+      radius: { value: 6, min: 1, max: 30, step: 1 },
     });
 
     const roundedRectShape = new THREE.Shape();
 
-    const x = 0;
+    const x = -30;
     const y = 0;
 
     roundedRectShape.moveTo(x, y + radius);
@@ -39,13 +79,14 @@ function App() {
     return roundedRectShape;
   }
 
-  const texture = useLoader(TextureLoader, "map2.png");
+  const textureMap = useLoader(TextureLoader, "trans2.png");
+  const colorMap = useLoader(TextureLoader, "mapColor.png");
 
-  const { colorFrame, opacity, countOfFrames } = useControls({
+  const { colorFrame, opacity, countOfFrames, backgroundColorFrame } = useControls({
     colorFrame: { value: "#7C52FF" },
-    // alphaMapText: {value: textureMap },
+    backgroundColorFrame: { value: "#000000" },
     opacity: {
-      value: 0.5,
+      value: 0.3,
       min: 0,
       max: 1,
       step: 0.001,
@@ -89,7 +130,7 @@ function App() {
   return (
     <Canvas camera={{ position: [90, 90, 100] }}>
       <OrbitControls enableDamping />
-      <color attach="background" args={["black"]} />
+      <color attach="background" args={[backgroundColorFrame]} />
       <ambientLight intensity={1} />
 
       <Instances
@@ -100,13 +141,16 @@ function App() {
           color={colorFrame}
           opacity={opacity}
           transparent={true}
+          alphaMap={textureMap}
         />
         <Instance scale={1} position={[0, 0, 10]} />
         <Instance scale={1} position={[0, 0, 20]} />
         <Instance scale={1} position={[0, 0, 30]} />
         <Instance scale={1} position={[0, 0, 40]} />
+        </Instances>
 
-      </Instances>
+
+
     </Canvas>
   );
 }
